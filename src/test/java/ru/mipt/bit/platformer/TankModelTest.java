c995c17895120f1a9d9e06ca62d7bd33ca8c91bd
22Nikitka22 <nalbulanov@yandex.ru>
2024-10-28 00:17:11 +0300

    fix test

--- src/test/java/ru/mipt/bit/platformer/TankModelTest.java
+++ src/test/java/ru/mipt/bit/platformer/TankModelTest.java
@@ -11,13 +11,13 @@ import ru.mipt.bit.platformer.objects.models.TankModel;
 import com.badlogic.gdx.math.GridPoint2;
 import ru.mipt.bit.platformer.objects.models.TreeModel;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 class TankModelTest {
 
     private TankModel tank;
-    private Collection<Obstacle> obstacles;
+    private Set<Obstacle> obstacles;
     private static final int ROW_COUNT = 5;
     private static final int COLUMN_COUNT = 5;
 
@@ -25,7 +25,7 @@ class TankModelTest {
     void setUp() {
         GridPoint2 initialCoordinates = new GridPoint2(2, 2);
         tank = new TankModel(initialCoordinates);
-        obstacles = new ArrayList<>();
+        obstacles = new HashSet<>();
     }
 
     @Test
@@ -42,16 +42,14 @@ class TankModelTest {
 
     @Test
     void testMoveWithObstacles() {
-        obstacles.add(new TreeModel(new GridPoint2(2, 3))); // Assuming TreeGraphic implements Obstacle
+        obstacles.add(new TreeModel(new GridPoint2(2, 3)));
         tank.move(Direction.UP, obstacles, ROW_COUNT, COLUMN_COUNT);
-        assertEquals(new GridPoint2(2, 2), tank.getCoordinates()); // Should not move
+        assertEquals(new GridPoint2(2, 2), tank.getCoordinates());
     }
 
     @Test
     void testMoveOutOfBounds() {
-        tank.move(Direction.LEFT, obstacles, ROW_COUNT, COLUMN_COUNT); // Move to (1, 2)
-        tank.move(Direction.LEFT, obstacles, ROW_COUNT, COLUMN_COUNT); // Move to (0, 2)
-        tank.move(Direction.LEFT, obstacles, ROW_COUNT, COLUMN_COUNT); // Move to (-1, 2) - out of bounds
-        assertEquals(new GridPoint2(0, 2), tank.getCoordinates()); // Should not move out of bounds
+        tank.move(Direction.RIGHT, obstacles, 3, 3);
+        assertEquals(new GridPoint2(2, 2), tank.getCoordinates());
     }
 }
